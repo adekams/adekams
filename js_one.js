75b6e730cbe86cc3b736a753eca8e79cafd27af0
AdenikeSalau <kifademi@gmail.com>
2020-03-22 03:45:20 +0100

    JavaScript task 1

---
+++ js_one.js
@@ -0,0 +1,11 @@
+let name = "Ade";
+
+let courses = ['HTML', 'CSS', 'JavaScript', 'Python'];
+
+console.log ("My name is: " + name + ". I registered for: " + courses);
+
+for (let i=0; i<=200; i++) {
+    if (i%2===0){
+        console.log(i);
+    }
+}
