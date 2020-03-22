4571d31691b54e0cb5481140fd9fed4143e7d195
Adenike Salau <54279390+adekams@users.noreply.github.com>
2020-03-22 09:52:56 +0100

    js_one.js

--- js_one.js
+++ js_one.js
@@ -4,7 +4,7 @@ let courses = ['HTML', 'CSS', 'JavaScript', 'Python'];
 
 console.log ("My name is: " + name + ". I registered for: " + courses);
 
-for (let i=0; i<=200; i++) {
+for (let i=1; i<=200; i++) {
     if (i%2===0){
         console.log(i);
     }
